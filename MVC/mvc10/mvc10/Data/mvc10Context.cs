05551a394682d4d819104cf6cd040829b9f8eda5
223-saif <saifmca19@gmail.com>
2022-01-20 23:23:37 +0530

    Mvc Project

---
+++ MVC/mvc10/mvc10/Data/mvc10Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using mvc10.Models;
+
+namespace mvc10.Data
+{
+    public class mvc10Context : DbContext
+    {
+        public mvc10Context (DbContextOptions<mvc10Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<mvc10.Models.Department> Department { get; set; }
+    }
+}
---
+++ MVC/mvc10/mvc10/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvc10.Models
+{
+    public class Department
+    {
+        public int ID { get; set; }
+        public string DeptName { get; set; }
+        public string Location { get; set; }
+        public int Num_Of_Staff { get; set; }
+    }
+}
