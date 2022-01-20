3d9a3a66c087f6156bb0dd6c36917f2f3d479c30
223-saif <saifmca19@gmail.com>
2022-01-20 22:58:33 +0530

    MVC

---
+++ MVC/MVC 10/MVC 10/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_10.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+
+        public string EmployeeName { get; set; }
+
+        public int Salary { get; set; }
+
+        public string Location { get; set; }
+    }
+}
