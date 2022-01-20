0a6d7a18b5e5693542aeafdbf7e123d19a488b51
~saif <96821946+md-saiff@users.noreply.github.com>
2022-01-20 15:57:19 +0530

    Add files via upload

---
+++ C#/ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Stack mystack = new Stack();
+            //  mystack.Push("Hell0 !");
+            //mystack.Push(null);
+            //mystack.Push(1);
+            // mystack.Push(2);
+            //mystack.Push(3);
+
+
+            Queue myqueue = new Queue();
+            myqueue.Enqueue(" Hello !");
+            myqueue.Enqueue(1);
+            myqueue.Enqueue(2);
+            myqueue.Enqueue(3);
+
+            foreach (var item in myqueue)
+        {
+            Console.WriteLine(item);
+        }
+        }
+    }
+}
