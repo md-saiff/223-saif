ab68aba175d041be21029a407f5b11a9b192b138
223-saif <saifmca19@gmail.com>
2022-01-24 12:57:11 +0530

    WebApi

---
+++ LMS-Assessment/WEB-API/Exercise-3/Data/Exercise_3Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Exercise_3.Model;
+
+namespace Exercise_3.Data
+{
+    public class Exercise_3Context : DbContext
+    {
+        public Exercise_3Context (DbContextOptions<Exercise_3Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Exercise_3.Model.Student> Student { get; set; }
+    }
+}
